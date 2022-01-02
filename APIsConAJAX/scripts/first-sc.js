30a3f8b9e8b775c5388b1f92bb2beff10c6e4b6a
= <=>
2022-01-01 22:38:09 -0300

    query params class

--- APIsConAJAX/scripts/first-sc.js
+++ APIsConAJAX/scripts/first-sc.js
@@ -1,5 +1,19 @@
 
-const query = fetch('https://jsonplaceholder.typicode.com/posts');
+
+let url = new URL('https://jsonplaceholder.typicode.com/posts');
+
+let parametros = {
+    userId: 1,
+    _limit: 5,
+}
+
+Object.keys(parametros).forEach(
+    paramKey => {
+        url.searchParams.append(paramKey, parametros[paramKey])
+    }
+);
+
+const query = fetch(url);
 
 console.log(query.then(
     function(response){
